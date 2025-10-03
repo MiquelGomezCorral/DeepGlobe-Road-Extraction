aad5647add6502e9e8a23dcc5b6822df5708ef6b
Miquel Gómez Corral <miquelgc2003@gmail.com>
2025-10-03 16:19:36 +0200

    fixed setup

--- app/setup.py
+++ app/setup.py
@@ -1,9 +1,9 @@
-"""setup.py.
-"""
-from setuptools import setup, find_packages
+"""setup.py."""
+from setuptools import find_packages, setup
 
 setup(
     name="app",
     version="0.1.0",
-    packages=find_packages(), 
+    packages=find_packages(),
+    package_dir={"": "."},
 )
