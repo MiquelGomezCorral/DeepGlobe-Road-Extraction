9273a497800c5ba294cad4297550a1c3bb3331bf
Miquel Gómez Corral <miquelgc2003@gmail.com>
2025-10-05 13:58:32 +0200

    added model config

--- app/src/config/__init__.py
+++ app/src/config/__init__.py
@@ -4,3 +4,4 @@ General app configuration with definitions of variables and values.
 """
 
 from .config import Configuration, args_to_config  # noqa: F401
+from .model_config import ModelConfiguration, args_to_model_config  # noqa: F401
---
+++ app/src/config/model_config.py
@@ -0,0 +1,50 @@
+"""ModelConfiguration file.
+
+Configuration of the model parameters and so on.
+"""
+import dataclasses
+from argparse import Namespace
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelConfiguration:
+    """Configuration class for the project.
+
+    This class contains all the configuration variables for the project.
+    """
+
+    n_training_samples: int = None
+    epochs: int = 10
+    batch_size: int = 4
+    max_steps: int = 1000
+    learning_rate: float = 0.0001
+
+    in_channels: int = 3
+    out_classes: int = 1
+
+    model_name: str = "Unet"
+    encoder_name: str = "resnet34"
+
+    def __post_init__(self):
+        """Post-initialization."""
+        if self.n_training_samples is not None:
+            self.max_steps = self.epochs * (self.n_training_samples // self.batch_size)
+
+
+def args_to_model_config(args: Namespace):
+    """From the args namespace, create a ModelConfiguration.
+
+    It will change all the fields that have ben added to the args.
+    If a field is not added in the args will be ignored.
+    Fields in the args that are not in the Config this will be ignored.
+
+    Args:
+        args (Namespace): Parsed arguments.
+
+    Returns:
+        ModelConfiguration: ModelConfiguration with args values.
+    """
+    fields = {f.name for f in dataclasses.fields(ModelConfiguration)}
+    filtered = {k: v for k, v in vars(args).items() if k in fields and v is not None}
+    return ModelConfiguration(**filtered)
